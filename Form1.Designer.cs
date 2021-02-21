f969e67bf2340fd3b48474ac0f92eecbccba30c1
fayeazahmed <r10.ahmed@yahoo.com>
2021-02-21 21:55:39 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace TorrentDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchLabel = new System.Windows.Forms.Label();
+            this.SearchText = new System.Windows.Forms.TextBox();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.ResultPanel = new System.Windows.Forms.Panel();
+            this.ResultPanelLabel = new System.Windows.Forms.Label();
+            this.ResultPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SearchLabel
+            // 
+            this.SearchLabel.AutoSize = true;
+            this.SearchLabel.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SearchLabel.ForeColor = System.Drawing.Color.Honeydew;
+            this.SearchLabel.Location = new System.Drawing.Point(29, 22);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(241, 29);
+            this.SearchLabel.TabIndex = 0;
+            this.SearchLabel.Text = "Search for something";
+            this.SearchLabel.Click += new System.EventHandler(this.SearchLabel_Click);
+            // 
+            // SearchText
+            // 
+            this.SearchText.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.SearchText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SearchText.Location = new System.Drawing.Point(276, 22);
+            this.SearchText.Multiline = true;
+            this.SearchText.Name = "SearchText";
+            this.SearchText.Size = new System.Drawing.Size(436, 29);
+            this.SearchText.TabIndex = 1;
+            this.SearchText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SearchText_KeyDown);
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SubmitBtn.Location = new System.Drawing.Point(719, 22);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(90, 29);
+            this.SubmitBtn.TabIndex = 2;
+            this.SubmitBtn.Text = "Search";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // ResultPanel
+            // 
+            this.ResultPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ResultPanel.AutoScroll = true;
+            this.ResultPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ResultPanel.Controls.Add(this.ResultPanelLabel);
+            this.ResultPanel.Location = new System.Drawing.Point(29, 81);
+            this.ResultPanel.Name = "ResultPanel";
+            this.ResultPanel.Size = new System.Drawing.Size(811, 505);
+            this.ResultPanel.TabIndex = 3;
+            // 
+            // ResultPanelLabel
+            // 
+            this.ResultPanelLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ResultPanelLabel.Font = new System.Drawing.Font("Siyam Rupali", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ResultPanelLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ResultPanelLabel.Location = new System.Drawing.Point(0, 0);
+            this.ResultPanelLabel.Name = "ResultPanelLabel";
+            this.ResultPanelLabel.Size = new System.Drawing.Size(811, 50);
+            this.ResultPanelLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1067, 622);
+            this.Controls.Add(this.ResultPanel);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.SearchText);
+            this.Controls.Add(this.SearchLabel);
+            this.MaximumSize = new System.Drawing.Size(1083, 661);
+            this.Name = "Form1";
+            this.Text = "Torrent Helper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResultPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.TextBox SearchText;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.Panel ResultPanel;
+        private System.Windows.Forms.Label ResultPanelLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorrentDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static WebBrowser browser = new WebBrowser();
+        public HtmlNodeCollection FindTorrents(string query)
+        {
+            string url = $"https://thepiratebay10.org/search/{query}/1/99/0";
+            browser.ScriptErrorsSuppressed = true;
+            browser.Navigate(url);
+
+            waitTillLoad(browser);
+
+            HtmlWindow window = browser.Document.Window;
+            string str = window.Document.Body.OuterHtml;
+
+            HtmlAgilityPack.HtmlDocument HtmlDoc = new HtmlAgilityPack.HtmlDocument();
+            HtmlDoc.LoadHtml(str); 
+            HtmlNode node = HtmlDoc.GetElementbyId("searchResult");
+            HtmlNodeCollection torrents = node.SelectNodes("//tbody//tr");
+            return torrents;
+        }
+
+        private static void waitTillLoad(WebBrowser webBrControl)
+        {
+            WebBrowserReadyState loadStatus;
+            int waittime = 100000;
+            int counter = 0;
+            while (true)
+            {
+                loadStatus = webBrControl.ReadyState;
+                Application.DoEvents();
+                if ((counter > waittime) || (loadStatus == WebBrowserReadyState.Uninitialized) || (loadStatus == WebBrowserReadyState.Loading) || (loadStatus == WebBrowserReadyState.Interactive))
+                {
+                    break;
+                }
+                counter++;
+            }
+
+            counter = 0;
+            while (true)
+            {
+                loadStatus = webBrControl.ReadyState;
+                Application.DoEvents();
+                if (loadStatus == WebBrowserReadyState.Complete && webBrControl.IsBusy != true)
+                {
+                    break;
+                }
+                counter++;
+            }
+        }
+
+        private void GenerateResults(int index, int panelY, HtmlNode torrent)
+        {
+            HtmlNode typeNode = torrent.SelectSingleNode(".//td[@class='vertTh']");
+            HtmlNode nameNode = torrent.SelectSingleNode(".//div[@class='detName']");
+            HtmlNode descNode = torrent.SelectSingleNode(".//font[@class='detDesc']");
+            HtmlNode seedNode = torrent.SelectSingleNode(".//td[3]");
+            HtmlNode leechNode = torrent.SelectSingleNode(".//td[4]");
+            HtmlNode magnetNode = torrent.SelectSingleNode(".//td[2]//a[@title='Download this torrent using magnet']");
+            string magnetLink = magnetNode.Attributes["href"].Value;
+
+            Panel panel = new Panel
+            {
+                Name = "panel" + index,
+                BackColor = Color.LightGray,
+                BorderStyle = BorderStyle.FixedSingle,
+                Location = new Point(0, panelY),
+                Size = new Size(770, 64)
+            };
+
+            TextBox typeBox = new TextBox
+            {
+                Font = new Font("Sitka Subheading", 15, FontStyle.Regular),
+                Text = typeNode.InnerText.Trim(),
+                Size = new Size(100, 64),
+                Multiline = true,
+                Location = new Point(5, 0),
+                ReadOnly = true
+            };
+
+            TextBox nameBox = new TextBox
+            {
+                Font = new Font("Sitka Subheading", 12, FontStyle.Bold),
+                Text = nameNode.InnerText.Trim(),
+                Size = new Size(373, 32),
+                Multiline = true,
+                Location = new Point(111, 0),
+                BackColor = Color.White,
+                ForeColor = Color.DarkBlue,
+                ReadOnly = true
+            };
+
+            string description = descNode.InnerText.Trim().Replace("&nbsp", " ");
+            TextBox descBox = new TextBox
+            {
+                Font = new Font("Sitka Subheading", 12, FontStyle.Regular),
+                Text = description,
+                Size = new Size(373, 32),
+                Multiline = true,
+                Location = new Point(111, 32),
+                ReadOnly = true
+            };
+
+            Button button = new Button
+            {
+                Size = new Size(75, 64),
+                Location = new Point(503, 0),
+                BackgroundImage = Properties.Resources.magnet,
+                BackgroundImageLayout = ImageLayout.Stretch
+            };
+            button.Click += (sender, e) => OpenMagnetLink(magnetLink.ToString());
+
+            Label seedLabel = new Label
+            {
+                Text = seedNode.InnerText,
+                ForeColor = Color.Green,
+                Location = new Point(590, 17),
+                Font = new Font("Segoe UI", 12, FontStyle.Regular),
+                AutoSize = true
+            };
+
+            Label leechLabel = new Label
+            {
+                Text = leechNode.InnerText,
+                ForeColor = Color.Red,
+                Location = new Point(640, 17),
+                Font = new Font("Segoe UI", 12, FontStyle.Regular),
+                AutoSize = true
+            };
+
+            panel.Controls.Add(typeBox);
+            panel.Controls.Add(nameBox);
+            panel.Controls.Add(descBox);
+            panel.Controls.Add(button);
+            panel.Controls.Add(seedLabel);
+            panel.Controls.Add(leechLabel);
+
+            ResultPanel.Controls.Add(panel);
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            string query = SearchText.Text;
+            if (query.Length > 0)
+            {
+                HtmlNodeCollection torrents = FindTorrents(query);
+                ResultPanel.Controls.Clear();
+                Label resultPanelLabel = new Label
+                {
+                    Font = new Font("Siyam Rupali", 15, FontStyle.Bold),
+                    ForeColor = Color.White,
+                    Size = new Size(790, 50),
+                    Location = new Point(0, 0)
+                };
+                if (torrents == null)
+                {
+                    resultPanelLabel.Text = "No result found for: " + query;
+                }
+                else
+                {
+                    int panelY = 40;
+                    resultPanelLabel.Text = "Search results for: " + query;
+                    for (int i = 0; i < torrents.Count - 1; i++)
+                    {
+                        GenerateResults(i, panelY, torrents[i]);
+                        panelY += 70;
+                    }
+                }
+                ResultPanel.Controls.Add(resultPanelLabel);
+                ResultPanel.Visible = true;
+                SearchText.Clear();
+            }
+        }
+
+        private void OpenMagnetLink(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void SearchText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                SubmitBtn.PerformClick();
+            }
+        }
+
+        private void SearchLabel_Click(object sender, EventArgs e)
+        {
+            SearchText.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
